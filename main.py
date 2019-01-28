27264300c4f677b23abf8f5e25a07fd3396d4981
Alexandre Augusto <alexandrebarbaruiva@users.noreply.github.com>
2019-01-28 12:57:37 -0200

    Add Travis CI pipeline and CodeClimate verification

--- main.py
+++ main.py
@@ -13,17 +13,25 @@ class Collector:
 
     def get_org_repos(self, org):
         o = self.g.get_organization(org)
+        print("get_org_repos")
         repos = [repo.full_name for repo in list(o.get_repos())]
         return repos
 
-    def get_all_contributors(self, repo):
-        r = self.g.get_repo(repo)
+    def get_all_contributors(self, org, repo):
+        url = org + repo
+        r = self.g.get_repo(url)
+        print("get_all_contributors")
         contributors = [user.login for user in list(r.get_contributors())]
         return contributors
 
-    def get_stats_contributors(self, repo):
-        r = self.g.get_repo(repo)
-        itens = [{item.author.login: item.raw_data['weeks']} for item in list(r.get_stats_contributors())]
+    def get_stats_contributors(self, org, repo):
+        url = org + repo
+        r = self.g.get_repo(url)
+        print("get_stats_contributors")
+        itens = [
+            {item.author.login: item.raw_data['weeks']}
+            for item in list(r.get_stats_contributors())
+        ]
         return itens
 
 
